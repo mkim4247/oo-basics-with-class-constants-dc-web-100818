b83bfb692721a3a05f3960bd59e59ef49e31c7e3
Michael Kim <m.kim4247@gmail.com>
2020-12-28 15:52:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
-      if !(BRANDS.include?(brand))
-        BRANDS << brand 
-      else 
-        BRANDS 
-      end 
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
